125a93d0a464b22b844f371dd969d1ad55be61fd
ridhianand16 <ridhianand16@gmail.com>
2020-03-20 00:41:43 +0530

    deleting 1

--- common/authUtils.mjs
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken';
-const newSessionRoutes = [{ path: '/user/login', method: 'POST' }];
-const authRoutes = [{ path: '/user/password', method: 'PUT' }];
-const SECRET_KEY = "JWT_SECRET";
